d47abe224ac1a23b5490625bb362715993a57a8f
m-e-l-u-h-a-n <pururshottam.tiwari.cd.cse19@itbhu.ac.in>
2020-10-05 22:01:41 +0530

    before firebase app initialisation

--- server/app.js
+++ server/app.js
@@ -5,7 +5,9 @@ import logger from 'morgan';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
+import { connect } from "./utils/db";
 
+connect() // set url and port in config.
 var app = express();
 
 app.use(logger('dev'));
@@ -16,5 +18,21 @@ app.use(express.Router(express.static(path.join(__dirname, '../public'))));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500).json({ message: err });
+  res.render('error');
+});
 
 export default app;
